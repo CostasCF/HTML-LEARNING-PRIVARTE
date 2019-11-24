a9139ead0794dc224d58c0a40eb1fa03218a6b25
Konstantinos Kalogeropoulos <kkalo878@gmail.com>
2019-11-24 13:57:18 +0200

    Added checkbox and >255 return false parametre

--- Π19057-04/myscripts.js
+++ Π19057-04/myscripts.js
@@ -21,7 +21,39 @@ function CheckForBlanks()
 		return false; /* Θελω η φορμα να μην στείλει τα στοιχεία NotSubmit, κάτι σαν break operation */
 	}
 }
+function CheckForBlanks2()
+{
+	if(document.getElementById('ip').value == "") {
+		alert("Παρακαλώ συμπληρώστε όλα τα υποχρεωτικά πεδία.");
+		document.getElementById('ip').style.borderColor = "red";
+		return false; /* Θελω η φορμα να μην στείλει τα στοιχεία NotSubmit, κάτι σαν break operation */
+	}
+	if(document.getElementById('NetMusk').value == "") {
+		alert("Παρακαλώ συμπληρώστε όλα τα υποχρεωτικά πεδία.");
+		document.getElementById('NetMusk').style.borderColor = "red";
+		return false; /* Θελω η φορμα να μην στείλει τα στοιχεία NotSubmit, κάτι σαν break operation */
+	}
+}
 
+function CheckforNum()
+{
+	if (document.getElementById('ip').value > 255.0){
+		alert("Παρακαλώ εισάγεται στοιχεία με τιμή μικρότερη του 255")
+		document.getElementById('ip').style.borderColor = "red";
+
+	}
+	if(document.getElementById('NetMusk').value > 255.0) {
+		alert("Παρακαλώ εισάγεται στοιχεία με τιμή μικρότερη του 255");
+		document.getElementById('NetMusk').style.borderColor = "red";
+	
+}
+}
+
+function dec2bin(){
+	document.getElementById("ip").value = dec2bin(-5);
+	document.write (ip >>> 0).toString(2);
+  return false;
+}
 
 // --------ΠΡΟΣΠΑΘΕΙΑ ΓΙΑ ΠΡΟΕΠΙΣΚΟΠΙΣΗ ΣΥΜΠΛΗΡΩΜΕΝΩΝ ΣΤΟΙΧΕΙΩΝ ΦΟΡΜΑΣ--------------
 //function onsubmit=myFunction(){
